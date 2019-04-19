d90afb1d46b756449aa6d265db0d0047decaf9a3
Matheus Diógenes Andrade <matheusdiogenesandrade@gmail.com>
2019-04-19 16:41:19 -0300

    First

---
+++ src/problems/qbf/solvers/GA_QBFPT.java
@@ -0,0 +1,282 @@
+package problems.qbf.solvers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import metaheuristics.ga.AbstractGA;
+import problems.qbf.QBF;
+import solutions.Solution;
+import triple.Triple;
+import triple.TripleElement;
+
+/**
+ * Metaheuristic GA (Genetic Algorithm) for
+ * obtaining an optimal solution to a QBF (Quadractive Binary Function --
+ * {@link #QuadracticBinaryFunction}). 
+ * 
+ * @author ccavellucci, fusberti
+ */
+public class GA_QBFPT extends AbstractGA<Integer, Integer> {
+
+	/**
+     * List of element objects used in prohibited triples. These objects
+     * represents the variables of the model.
+     */
+    private TripleElement[] tripleElements;
+
+    /**
+     * List of prohibited triples.
+     */
+    private Triple[] triples; 
+    
+    /**
+	 * the Candidate List of elements to enter the solution.
+	 */
+	protected ArrayList<Integer> CL;
+	
+	/**
+	 * Constructor for the GA_QBF class. The QBF objective function is passed as
+	 * argument for the superclass constructor.
+	 * 
+	 * @param generations
+	 *            Maximum number of generations.
+	 * @param popSize
+	 *            Size of the population.
+	 * @param mutationRate
+	 *            The mutation rate.
+	 * @param filename
+	 *            Name of the file for which the objective function parameters
+	 *            should be read.
+	 * @throws IOException
+	 *             Necessary for I/O operations.
+	 */
+	public GA_QBFPT(Integer generations, Integer popSize, Double mutationRate, String filename) throws IOException {
+		super(new QBF(filename), generations, popSize, mutationRate);
+		generateTripleElements();
+        generateTriples();
+	}
+
+	/**
+     * Linear congruent function l used to generate pseudo-random numbers.
+     */
+    public int l(int pi1, int pi2, int u, int n) {
+        return 1 + ((pi1 * u + pi2) % n);
+    }
+
+    /**
+     * Function g used to generate pseudo-random numbers
+     */
+    public int g(int u, int n) {
+        int pi1 = 131;
+        int pi2 = 1031;
+        int lU = l(pi1, pi2, u, n);
+
+        if (lU != u) {
+            return lU;
+        } else {
+            return 1 + (lU % n);
+        }
+    }
+
+    /**
+     * Function h used to generate pseudo-random numbers
+     */
+    public int h(int u, int n) {
+        int pi1 = 193;
+        int pi2 = 1093;
+        int lU = l(pi1, pi2, u, n);
+        int gU = g(u, n);
+
+        if (lU != u && lU != gU) {
+            return lU;
+        } else if ((1 + (lU % n)) != u && (1 + (lU % n)) != gU) {
+            return 1 + (lU % n);
+        } else {
+            return 1 + ((lU + 1) % n);
+        }
+    }
+
+    /**
+     * Method that generates a list of n prohibited triples using l g and h
+     * functions
+     */
+    private void generateTriples() {
+        int n = ObjFunction.getDomainSize();
+        this.triples = new Triple[ObjFunction.getDomainSize()];
+
+        for (int u = 1; u <= n; u++) {
+            TripleElement te1, te2, te3;
+            Triple newTriple;
+
+            te1 = tripleElements[u - 1];
+            te2 = tripleElements[g(u - 1, n) - 1];
+            te3 = tripleElements[h(u - 1, n) - 1];
+            newTriple = new Triple(te1, te2, te3);
+            
+            //Sorting new triple
+            Arrays.sort(newTriple.getElements(), Comparator.comparing(TripleElement::getIndex));
+
+            //newTriple.printTriple();
+            this.triples[u-1] = newTriple;
+        }
+    }
+    
+    /**
+     * That method generates a list of objects (Triple Elements) that represents
+     * each binary variable that could be inserted into a prohibited triple
+     */
+	private void generateTripleElements() {
+        int n = ObjFunction.getDomainSize();
+        this.tripleElements = new TripleElement[n];
+
+        for (int i = 0; i < n; i++) {
+            tripleElements[i] = new TripleElement(i);
+        }
+    }
+	
+	/**
+     * Update the list of candidates, keeping only those who
+     * can be inserted in the incumbent solution without violating
+     * any triple
+     */
+    public void updateCL(Chromosome chromosome) {
+        ArrayList<Integer> _CL = new ArrayList<Integer>();
+        
+        // Set all elements as available
+        for (TripleElement tripElem : this.tripleElements) {
+        	tripElem.setSelected(false);
+        	tripElem.setAvailable(true);
+        }
+        
+        // Set all incumbent elements as unavailable and selected
+        if (chromosome != null) {
+        	for (int i = 0; i < chromosomeSize; i++) {
+        		Integer var = chromosome.get(i);
+                this.tripleElements[i].setSelected(var == 1);
+                this.tripleElements[i].setAvailable(var == 0);
+            }
+        }
+        
+        // Set to unavailable those elements that can violate at least one triple
+        for (Triple trip : this.triples) {
+            TripleElement te0, te1, te2;
+            te0 = trip.getElements()[0];
+            te1 = trip.getElements()[1];
+            te2 = trip.getElements()[2];
+
+            if (te0.getSelected() && te1.getSelected()) {
+                te2.setAvailable(false);
+            } else if (te0.getSelected() && te2.getSelected()) {
+                te1.setAvailable(false);
+            } else if (te1.getSelected() && te2.getSelected()) {
+                te0.setAvailable(false);
+            }
+        }
+
+        // Update all CL with only available elements
+        for (TripleElement tripElem : this.tripleElements) {
+            if (!tripElem.getSelected() && tripElem.getAvailable()) {
+                _CL.add(tripElem.getIndex());
+            }
+        }
+
+        this.CL = _CL;
+    }
+	
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * This createEmptySol instantiates an empty solution and it attributes a
+	 * zero cost, since it is known that a QBF solution with all variables set
+	 * to zero has also zero cost.
+	 */
+	@Override
+	public Solution<Integer> createEmptySol() {
+		Solution<Integer> sol = new Solution<Integer>();
+		sol.cost = 0.0;
+		return sol;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see metaheuristics.ga.AbstractGA#decode(metaheuristics.ga.AbstractGA.
+	 * Chromosome)
+	 */
+	@Override
+	protected Solution<Integer> decode(Chromosome chromosome) {
+
+		Solution<Integer> solution = createEmptySol();
+		for (int locus = 0; locus < chromosome.size(); locus++) {
+			if (chromosome.get(locus) == 1) {
+				solution.add(new Integer(locus));
+			}
+		}
+
+		ObjFunction.evaluate(solution);
+		return solution;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see metaheuristics.ga.AbstractGA#generateRandomChromosome()
+	 */
+	@Override
+	protected Chromosome generateRandomChromosome() {
+
+		Chromosome chromosome = new Chromosome();
+		for (int i = 0; i < chromosomeSize; i++) {			
+			chromosome.add(rng.nextInt(2));
+			updateCL(chromosome);
+		}
+
+		return chromosome;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see metaheuristics.ga.AbstractGA#fitness(metaheuristics.ga.AbstractGA.
+	 * Chromosome)
+	 */
+	@Override
+	protected Double fitness(Chromosome chromosome) {
+
+		return decode(chromosome).cost;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * metaheuristics.ga.AbstractGA#mutateGene(metaheuristics.ga.AbstractGA.
+	 * Chromosome, java.lang.Integer)
+	 */
+	@Override
+	protected void mutateGene(Chromosome chromosome, Integer locus) {
+//		check broken triples
+		chromosome.set(locus, 1 - chromosome.get(locus));
+		
+	}
+
+	/**
+	 * A main method used for testing the GA metaheuristic.
+	 * 
+	 */
+	public static void main(String[] args) throws IOException {
+
+		long startTime = System.currentTimeMillis();
+		GA_QBFPT ga = new GA_QBFPT(1000, 100, 1.0 / 100.0, "instances/qbf100");
+		Solution<Integer> bestSol = ga.solve();
+		System.out.println("maxVal = " + bestSol);
+		long endTime = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println("Time = " + (double) totalTime / (double) 1000 + " seg");
+
+	}
+
+}

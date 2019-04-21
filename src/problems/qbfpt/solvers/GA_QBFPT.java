fcacb2a6299d84602b150a85171ac5025f2cace3
jonatastbelotti <Belotti@DESKTOP-K3FRGDC>
2019-04-21 15:59:22 -0300

    Adição das triplas probidas

--- src/problems/qbfpt/solvers/GA_QBFPT.java
+++ src/problems/qbfpt/solvers/GA_QBFPT.java
@@ -6,7 +6,14 @@
 package problems.qbfpt.solvers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import metaheuristics.ga.Chromosome;
 import problems.qbf.solvers.GA_QBF;
+import problems.qbfpt.Triple;
+import problems.qbfpt.TripleElement;
+import solutions.Solution;
 
 /**
  *
@@ -14,8 +21,164 @@ import problems.qbf.solvers.GA_QBF;
  */
 public class GA_QBFPT extends GA_QBF {
 
+    /**
+     * List of element objects used in prohibited triples. These objects
+     * represents the variables of the model.
+     */
+    private TripleElement[] tripleElements;
+
+    /**
+     * List of prohibited triples.
+     */
+    private Triple[] triples;
+    
+    /**
+     * the Candidate List of elements to enter the solution.
+     */
+    protected ArrayList<Integer> CL;
+
     public GA_QBFPT(Integer tempoExecucao, Integer geracoesConvengencia, Integer popSize, Double mutationRate, String filename) throws IOException {
         super(tempoExecucao, geracoesConvengencia, popSize, mutationRate, filename);
+
+        generateTripleElements();
+        generateTriples();
+    }
+
+    /**
+     * Linear congruent function l used to generate pseudo-random numbers.
+     */
+    public int l(int pi1, int pi2, int u, int n) {
+        return 1 + ((pi1 * u + pi2) % n);
+    }
+
+    /**
+     * Function g used to generate pseudo-random numbers
+     */
+    public int g(int u, int n) {
+        int pi1 = 131;
+        int pi2 = 1031;
+        int lU = l(pi1, pi2, u, n);
+
+        if (lU != u) {
+            return lU;
+        } else {
+            return 1 + (lU % n);
+        }
+    }
+
+    /**
+     * Function h used to generate pseudo-random numbers
+     */
+    public int h(int u, int n) {
+        int pi1 = 193;
+        int pi2 = 1093;
+        int lU = l(pi1, pi2, u, n);
+        int gU = g(u, n);
+
+        if (lU != u && lU != gU) {
+            return lU;
+        } else if ((1 + (lU % n)) != u && (1 + (lU % n)) != gU) {
+            return 1 + (lU % n);
+        } else {
+            return 1 + ((lU + 1) % n);
+        }
+    }
+
+    /**
+     * That method generates a list of objects (Triple Elements) that represents
+     * each binary variable that could be inserted into a prohibited triple
+     */
+    private void generateTripleElements() {
+        int n = ObjFunction.getDomainSize();
+        this.tripleElements = new TripleElement[n];
+
+        for (int i = 0; i < n; i++) {
+            tripleElements[i] = new TripleElement(i);
+        }
+    }
+
+    /**
+     * Method that generates a list of n prohibited triples using l g and h
+     * functions
+     */
+    private void generateTriples() {
+        int n = ObjFunction.getDomainSize();
+        this.triples = new Triple[ObjFunction.getDomainSize()];
+
+        for (int u = 1; u <= n; u++) {
+            TripleElement te1, te2, te3;
+            Triple novaTripla;
+
+            te1 = tripleElements[u - 1];
+            te2 = tripleElements[g(u - 1, n) - 1];
+            te3 = tripleElements[h(u - 1, n) - 1];
+            novaTripla = new Triple(te1, te2, te3);
+
+            Collections.sort(novaTripla.getElements(), new Comparator<TripleElement>() {
+                public int compare(TripleElement te1, TripleElement te2) {
+                    return te1.getIndex().compareTo(te2.getIndex());
+                }
+            });
+            //novaTripla.printTriple();
+            this.triples[u - 1] = novaTripla;
+        }
+    }
+    
+    /**
+     * A GRASP CL generator for MAXQBFPT problem
+     *
+     * @return A list of candidates to partial solution
+     */
+    public ArrayList<Integer> makeCL() {
+        int n = ObjFunction.getDomainSize();
+        ArrayList<Integer> _CL = new ArrayList<Integer>(n);
+
+        for (TripleElement tripElem : this.tripleElements) {
+            tripElem.setAvailable(true);
+            tripElem.setSelected(false);
+            _CL.add(tripElem.getIndex());
+        }
+
+        return _CL;
+    }
+
+    /**
+     * The CL updater for MAXQBFPT problem
+     *
+     */
+    public void updateCL(Chromosome<Integer> cro) {
+        ArrayList<Integer> _CL = new ArrayList<Integer>();
+        Solution<Integer> solAtual = decode(cro);
+
+        if (solAtual != null) {
+            for (Integer e : solAtual) {
+                this.tripleElements[e].setSelected(true);
+                this.tripleElements[e].setAvailable(false);
+            }
+        }
+
+        for (Triple trip : this.triples) {
+            TripleElement te0, te1, te2;
+            te0 = trip.getElements().get(0);
+            te1 = trip.getElements().get(1);
+            te2 = trip.getElements().get(2);
+
+            if (te0.getSelected() && te1.getSelected()) {
+                te2.setAvailable(false);
+            } else if (te0.getSelected() && te2.getSelected()) {
+                te1.setAvailable(false);
+            } else if (te1.getSelected() && te2.getSelected()) {
+                te0.setAvailable(false);
+            }
+        }
+
+        for (TripleElement tripElem : this.tripleElements) {
+            if (!tripElem.getSelected() && tripElem.getAvailable()) {
+                _CL.add(tripElem.getIndex());
+            }
+        }
+
+        this.CL = _CL;
     }
 
 }
